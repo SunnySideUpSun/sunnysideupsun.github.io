ebabf4fd762cf4ecef69fd0c61fd1369e1c762c2
SunnySideUpSun <78585950+SunnySideUpSun@users.noreply.github.com>
2022-06-28 00:50:01 +0800

    Updates

---
+++ precache-manifest.c4c8d81d8e1a23220f5bf68ada8d1408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f907edf8cc6baf034e3b3779cb0fb314",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6807d978be0aefd548",
+    "url": "/static/css/2.6f4ee0a7.chunk.css"
+  },
+  {
+    "revision": "eb2dcc01af239f1e8756",
+    "url": "/static/css/main.8653b40b.chunk.css"
+  },
+  {
+    "revision": "ac6807d978be0aefd548",
+    "url": "/static/js/2.c95ff662.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.c95ff662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb2dcc01af239f1e8756",
+    "url": "/static/js/main.d86f90cd.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
